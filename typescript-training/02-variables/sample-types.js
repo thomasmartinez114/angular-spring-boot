cd8c5b57cd04f9f9c544068ac2c213128fa941d4
Thomas Martinez <tmartinez114@gmail.com>
2021-10-08 23:58:36 -0400

    defining variables

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,14 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Anup";
+var lastName = "Kumar";
+// let's break it
+// found = 0;
+// grade = "A";
+// firstName = false;
+// lastName = 2;
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// use template string
+console.log("Hi " + firstName + " " + lastName);
---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,18 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Anup";
+let lastName: string = "Kumar";
+
+// let's break it
+
+// found = 0;
+// grade = "A";
+// firstName = false;
+// lastName = 2;
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+// use template string
+console.log(`Hi ${firstName} ${lastName}`)
