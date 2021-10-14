3f295455415394db31d14c898225cb70cb26101f
Thomas Martinez <tmartinez114@gmail.com>
2021-10-13 22:56:51 -0400

    tsconfig will compile all ts files in dir as well, wont need to list them in cmd

---
+++ typescript-training/04-classes/03-tsconfig/Customer.js
@@ -0,0 +1,32 @@
+"use strict";
+var Customer = /** @class */ (function () {
+    function Customer(theFirst, theLast) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
+// create instance of the class
+var myCustomer = new Customer("Martin", "Dixon");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
