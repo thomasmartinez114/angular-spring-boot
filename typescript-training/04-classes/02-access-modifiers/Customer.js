7e58481dcb2e49296b1d66393c7bf79de29b99ee
Thomas Martinez <tmartinez114@gmail.com>
2021-10-13 22:50:21 -0400

    Accessors -- Getters/Setters

--- typescript-training/04-classes/02-access-modifiers/Customer.js
+++ typescript-training/04-classes/02-access-modifiers/Customer.js
@@ -1,8 +1,28 @@
 var Customer = /** @class */ (function () {
     function Customer(theFirst, theLast) {
-        this.firstName = theFirst;
-        this.lastName = theLast;
+        this._firstName = theFirst;
+        this._lastName = theLast;
     }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Customer;
 }());
 // create instance of the class
