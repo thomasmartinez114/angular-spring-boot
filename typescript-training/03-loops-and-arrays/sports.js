61e0b5d36a8bc517431707738943245cbc29e149
Thomas Martinez <tmartinez114@gmail.com>
2021-10-13 20:12:26 -0400

    conditional with arr

--- typescript-training/03-loops-and-arrays/sports.js
+++ typescript-training/03-loops-and-arrays/sports.js
@@ -1,4 +1,10 @@
 var sportsOne = ["Golf", "Cricket", "Tennis", "Swimming"];
-for (var i = 0; i < sportsOne.length; i++) {
-    console.log(sportsOne[i]);
+for (var _i = 0, sportsOne_1 = sportsOne; _i < sportsOne_1.length; _i++) {
+    var tempSport = sportsOne_1[_i];
+    if (tempSport == "Cricket") {
+        console.log(tempSport + "<< My Favorite!");
+    }
+    else {
+        console.log(tempSport);
+    }
 }
--- typescript-training/03-loops-and-arrays/sports.ts
+++ typescript-training/03-loops-and-arrays/sports.ts
@@ -1,5 +1,11 @@
 let sportsOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
 
-for (let i=0; i < sportsOne.length; i++) {
-    console.log(sportsOne[i]);
+for (let tempSport of sportsOne) {
+
+    if(tempSport == "Cricket") {
+        console.log(tempSport + "<< My Favorite!");
+    }
+    else {
+        console.log(tempSport);
+    }
 }
