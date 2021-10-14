474dcd0385216ba1ddf4e1e4264b3efe54335ad3
Thomas Martinez <tmartinez114@gmail.com>
2021-10-13 23:25:28 -0400

    Export/Import Classes

---
+++ typescript-training/04-classes/05-modules/Driver.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Customer_1 = require("./Customer");
+// create instance of the class
+var myCustomer = new Customer_1.Customer("Martin", "Dixon");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ typescript-training/04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { Customer } from "./Customer";
+
+// create instance of the class
+let myCustomer = new Customer("Martin", "Dixon");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
