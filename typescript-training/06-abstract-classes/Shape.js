3927156c0b862d79753bd346fc96dd0bd2f67031
Thomas Martinez <tmartinez114@gmail.com>
2021-10-15 19:43:24 -0400

    started abstract classes

---
+++ typescript-training/06-abstract-classes/Shape.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Shape = void 0;
+var Shape = /** @class */ (function () {
+    // Constructor
+    function Shape(_x, _y) {
+        this._x = _x;
+        this._y = _y;
+    }
+    Object.defineProperty(Shape.prototype, "x", {
+        // Getters and Setters
+        get: function () {
+            return this._x;
+        },
+        set: function (value) {
+            this._x = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Shape.prototype, "y", {
+        get: function () {
+            return this._y;
+        },
+        set: function (value) {
+            this._y = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    // Method
+    Shape.prototype.getInfo = function () {
+        return "x=" + this._x + ", y=" + this._y;
+    };
+    return Shape;
+}());
+exports.Shape = Shape;
---
+++ typescript-training/06-abstract-classes/Shape.ts
@@ -0,0 +1,34 @@
+export class Shape {
+
+    // Constructor
+
+    constructor(private _x: number, private _y: number) {
+
+    }
+
+    // Getters and Setters
+    
+    public get x(): number {
+        return this._x;
+    }
+
+    public set x(value: number) {
+        this._x = value;
+    }
+
+    public get y(): number {
+        return this._y;
+    }
+
+    public set y(value: number) {
+        this._y = value;
+    }
+
+    // Method
+
+    getInfo(): string {
+        return `x=${this._x}, y=${this._y}`;
+    }
+
+
+}
