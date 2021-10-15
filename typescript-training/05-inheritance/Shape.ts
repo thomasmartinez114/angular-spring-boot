89d51d73f1072b462095bb49126b629506c10da5
Thomas Martinez <tmartinez114@gmail.com>
2021-10-15 18:52:17 -0400

    create Shape.ts

---
+++ typescript-training/05-inheritance/Shape.ts
@@ -0,0 +1,37 @@
+export class Shape {
+
+    private _x: number;
+    private _y: number;
+
+    // Constructor
+
+    constructor(private _x: number, private _y: number) {
+
+    }
+
+    // Getters and Setters
+    
+    public get x(): number {
+        return this._x;
+    }
+
+    public set x(value: number) {
+        this._x = value;
+    }
+
+    public get y(): number {
+        return this._y;
+    }
+
+    public set y(value: number) {
+        this._y = value;
+    }
+
+    // Method
+
+    getInfo(): string {
+        return `x=${this._x}, y=${this._y}`;
+    }
+
+
+}
