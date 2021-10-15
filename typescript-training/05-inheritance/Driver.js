02ece59a634868ba1fd3be803eace677d4944a2e
Thomas Martinez <tmartinez114@gmail.com>
2021-10-15 19:22:20 -0400

    Rectangle.ts

--- typescript-training/05-inheritance/Driver.js
+++ typescript-training/05-inheritance/Driver.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Shape_1 = require("./Shape");
 var Circle_1 = require("./Circle");
+var Rectangle_1 = require("./Rectangle");
 var myShape = new Shape_1.Shape(10, 15);
 console.log(myShape.getInfo());
 var myCircle = new Circle_1.Circle(5, 10, 20);
 console.log(myCircle.getInfo());
+var myRectangle = new Rectangle_1.Rectangle(0, 0, 3, 7);
+console.log(myRectangle.getInfo());
